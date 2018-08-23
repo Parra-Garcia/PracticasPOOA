43e728def3ae8f764eacbc29098a0ece0e645b9e
Alumno <Alumno@ws204-08>
2018-08-22 18:50:28 -0700

    test pa que hubiese error

--- src/pack1/test.java
+++ src/pack1/test.java
@@ -6,11 +6,11 @@ public class test {
 		// TODO Auto-generated method stub
 	System.out.println(Operaciones.MULTIPLICACION);
 	Operaciones.SUMA.name();
-	
+	Operaciones.RESTA.name();
 	
 	for(Operaciones elemento:Operaciones.values()){
 		System.out.println(elemento.getValor());
-		System.out.println(elemento.name());
+		
 	}
 	}
 	
