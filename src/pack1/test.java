39aa989bf11d0e1a66c35d97f504ce2825a2a1df
Alumno <Alumno@ws204-08>
2018-08-22 18:52:07 -0700

    aver como generar error

--- src/pack1/test.java
+++ src/pack1/test.java
@@ -6,10 +6,11 @@ public class test {
 		// TODO Auto-generated method stub
 	System.out.println(Operaciones.MULTIPLICACION);
 	Operaciones.SUMA.name();
-	Operaciones.RESTA.name();
+	
 	
 	for(Operaciones elemento:Operaciones.values()){
 		System.out.println(elemento.getValor());
+		Operaciones.RESTA.name();
 		
 	}
 	}
