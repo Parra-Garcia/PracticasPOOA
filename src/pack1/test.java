fbce05584e274d2c35aa52fd0eec283a77569d90
Alumno <Alumno@ws204-08>
2018-08-22 18:36:06 -0700

    se agrego un push

--- src/pack1/test.java
+++ src/pack1/test.java
@@ -6,6 +6,11 @@ public class test {
 		// TODO Auto-generated method stub
 	System.out.println(Operaciones.MULTIPLICACION);
 	Operaciones.SUMA.name();
+	
+	
+	for(Operaciones elemento:Operaciones.values()){
+		System.out.println(elemento.getValor());
+	}
 	}
 	
 }
