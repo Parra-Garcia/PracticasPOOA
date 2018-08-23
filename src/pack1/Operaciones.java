cd6e3fd51438d2ebb97e866e75d1eb100105cb7c
Alumno <Alumno@ws204-08>
2018-08-22 18:20:05 -0700

    Agregar Nuevo atributo (error en operaciones)

--- src/pack1/Operaciones.java
+++ src/pack1/Operaciones.java
@@ -1,6 +1,15 @@
 package pack1;
 
 public enum Operaciones {
-	SUMA, RESTA, MULTIPLICACION, DIVISION //variables declaradas
+	SUMA('+'), RESTA('-'), MULTIPLICACION('*'), DIVISION('/') //variables declaradas
 	
+	private String valor;
+	
+	Operaciones(String valor){ //no ponerle al construcctor identificadores de acceso ya que no se pueden hacer objetos de el
+		 this.valor = valor;
+		 
+	}
+	public String getValor(){
+		return this.valor;
+	}
 }
